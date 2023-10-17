0a096d7a61baf857dfe807c7b502f69da022a667
PolitoLimon <pfenori@fp.insjoaquimmir.cat>
2023-10-17 20:32:27 +0200

    Polito UwU

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + basedir + "/database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + basedir + "/base_de_datos.db"
 
 
 db = SQLAlchemy()
@@ -14,29 +14,54 @@ db.init_app(app)
 
 
 class Productes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    unitats = db.Column(db.Integer)
-    photo = db.Column(db.String(255))  
-    price = db.Column(db.Float)
-    category_id = db.Column(db.Integer)
-    seller_id = db.Column(db.Integer)
-    created = db.Column(db.String(10))  
-    updated = db.Column(db.String(10))  
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    photo = db.Column(db.String)
+    price = db.Column(db.Integer)
+    # category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    # seller_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created = db.Column(db.DateTime, nullable=False, server_default= db.func.now())
+    updated = db.Column(db.DateTime, nullable=False, server_default= db.func.now(), onupdate= db.func.now())
 
-
-
-@app.route('products/list')
-def mostrar_list():
-    return render_template('list.html', Productes=Productes)
 
 @app.route('/')
 def init():
-    return 'Hello, World!'
+    return redirect(url_for('list'))
 
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
+@app.route('/product')
+def list():
+    products = db.session.execute(
+        db.select(Productes).order_by(Productes.id.asc())
+    ).scalars()
+    return render_template('/products/list.html', products = products)
+
+@app.route('/product/<int:product_id>',methods = ['POST', 'GET'])
+def item(product_id):
+    # recupero l'item per la pk
+    product = db.session.get(Productes, product_id)
+    
+    if request.method == 'GET':
+        return render_template('item_update.html', product = Productes)
+    else: # POST
+        nom = request.form['nom']
+        unitats = int(request.form['unitats']) # es text, el passo a enter
+
+        # actualitzo els valors de l'item
+        product.nom = nom
+        product.unitats = unitats
+
+        # notifico que item ha canviat i amb el commit és guarda a la BBDD
+        db.session.add(item)
+        db.session.commit()
+
+        # https://en.wikipedia.org/wiki/Post/Redirect/Get
+        return redirect(url_for('item', product_id = product_id))
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
